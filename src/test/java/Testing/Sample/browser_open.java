fb078cb188a61c7caa4e953b2dc31023379291e3
Gagandeepgagan <gagandeepbains49@gmail.com>
2018-12-28 15:50:39 +0530

    manen project on jenkins

---
+++ src/test/java/Testing/Sample/browser_open.java
@@ -0,0 +1,15 @@
+package Testing.Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class browser_open {
+	
+
+	@Test
+	public void meth() {
+
+		System.out.println("gagan");
+	}
+}
